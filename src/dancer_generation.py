ca0966ddcce6696273d64de266b9d36044514970
Hendrik-Amann <100995431+Hendrik-Amann@users.noreply.github.com>
2023-12-12 17:25:31 +0100

    Update dancer_generation.py
    - change tokenizer.decode call

--- src/dancer_generation.py
+++ src/dancer_generation.py
@@ -35,12 +35,13 @@ def generate_summaries(test_loader, args, device):
             #return_dict_in_generate=True,
             #output_scores=True
         )  # only one beam should be equivalent to greedy,
+        
         gen_sum = [
             tokenizer.decode(
                 g, skip_special_tokens=True,
                 #HA: originally set to False, but I set it to True. It removes redundant white spaces
-                #HA: originally sent_outputs["sequences"] because it returend a dict, but I changed it to only returning the tensors, so it had to be changed to sent_outputs[0]
-                clean_up_tokenization_spaces=True) for g in sent_outputs[0]]
+                #HA: originally sent_outputs["sequences"] because it returend a dict, but I changed it to only returning the tensors, so it had to be changed to sent_outputs
+                clean_up_tokenization_spaces=False) for g in sent_outputs]
 
         gen_sums += gen_sum
         target_sums += batch[args.summary_column]
