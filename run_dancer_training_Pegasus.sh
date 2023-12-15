ba6c177802afd4006049303e3fe9e1aa655c6beb
Hendrik-Amann <100995431+Hendrik-Amann@users.noreply.github.com>
2023-12-15 19:38:35 +0100

    Update and rename run_dancer_training_Pegasus to run_dancer_training_Pegasus.sh

---
+++ run_dancer_training_Pegasus.sh
@@ -0,0 +1,18 @@
+python src/run_summarization.py \
+    --model_name_or_path google/pegasus-large --tokenizer_name google/pegasus-large \
+    --do_train \
+    --do_eval \
+    --task summarization \
+    --dataset_name Hendrik-a/dancer-data \
+    --text_column document \
+    --summary_column summary \
+    --seed 100 \
+    --per_device_train_batch_size=32 \
+    --per_device_eval_batch_size=32 \
+    --predict_with_generate \
+    --learning_rate 1e-4 \
+    --adafactor \
+    --max_source_length 1024 --max_target_length 128 --val_max_target_length 128 --pad_to_max_length True \
+    --num_beams 1 \
+    --save_step 300
+    --tf32 True
