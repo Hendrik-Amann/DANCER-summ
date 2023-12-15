75b6da0e8a8ddb0c3e45f6d9d7b9a00a3e665392
Hendrik-Amann <100995431+Hendrik-Amann@users.noreply.github.com>
2023-12-15 19:51:56 +0100

    Update run_dancer_training_Pegasus.sh

--- run_dancer_training_Pegasus.sh
+++ run_dancer_training_Pegasus.sh
@@ -7,12 +7,18 @@ python src/run_summarization.py \
     --text_column document \
     --summary_column summary \
     --seed 100 \
-    --per_device_train_batch_size=32 \
-    --per_device_eval_batch_size=32 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
     --predict_with_generate \
     --learning_rate 1e-4 \
     --adafactor \
     --max_source_length 1024 --max_target_length 128 --val_max_target_length 128 --pad_to_max_length True \
     --num_beams 1 \
-    --save_step 300
-    --tf32 True
+    --tf32 True \
+    --lr_scheduler_type constant \
+    --evaluation_strategy steps \
+    --save_strategy steps \
+    --save_steps 300 \
+    --eval_steps 300 \
+    --logging_steps 100 
+    
