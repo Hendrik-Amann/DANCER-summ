7d12b68db10ae4fcba0adfaa7148f0836ef702bf
Hendrik-Amann <100995431+Hendrik-Amann@users.noreply.github.com>
2023-12-25 12:35:31 +0100

    Update run_pegasusX_gen.sh

--- run_pegasusX_gen.sh
+++ run_pegasusX_gen.sh
@@ -4,16 +4,16 @@ python src/dancer_generation.py \
     --model_revision be2f12575b4d281f7f584ea40b8bf6702e1394e6 \
     --tokenizer_name google/pegasus-x-base \
     --tokenizer_revision cd8a69a4c88a469312423199b8dcd331f6af8b08 \
-    --output_path pX \
+    --max_source_length 16834 \
+    --max_target_length 256 \
+    --output_path genPX \
     --dataset_name Hendrik-a/arxiv \
     --text_column article_text \
     --summary_column abstract_text \
     --write_rouge 1 \
     --seed 100 \
     --test_batch_size 8 \
-    --max_source_length 16384 --max_summary_length 256 \
-    --num_beams 1 \
-    --no_repeat_ngram_size 0 \
-    --length_penalty 1.0 \
-    --split validation
-    
+    --num_beams 2 \
+    --length_penalty 1.2 \
+    --no_repeat_ngram_size 4 \
+    --max_test_samples 16
