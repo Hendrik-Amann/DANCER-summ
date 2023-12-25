ef825a38958a13bee965bd2d8bab24251678bb65
Hendrik-Amann <100995431+Hendrik-Amann@users.noreply.github.com>
2023-12-25 22:56:02 +0100

    Update run_dancer_gen.sh

--- run_dancer_gen.sh
+++ run_dancer_gen.sh
@@ -11,7 +11,7 @@ python src/dancer_generation.py \
   --text_column document \
   --summary_column summary \
   --write_rouge 1 \
-  --seed 100 \
+  --seed 10 \
   --test_batch_size 32 \
   --num_beams 2 \
   --length_penalty 1.2 \
