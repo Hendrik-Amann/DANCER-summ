bad737656acbe53622efed6971352e4f076cdeaa
Hendrik-Amann <100995431+Hendrik-Amann@users.noreply.github.com>
2023-12-26 08:37:31 +0100

    Update eval.py -correct partial

--- src/eval.py
+++ src/eval.py
@@ -81,7 +81,7 @@ def main():
     tdf = tdf.dropna()
     for index, text_row in tdf.iterrows():
       bp, br, bf1 = bert.score(cands=[text_row['gen_sum']], refs=[text_row['target_sum']])
-      score_row = {'article_id': text_row['article_id'], "BertP": bp.item(), "BertR": br.item(), "BertF1": bf1.item()}
+      score_row = {"article_id": text_row["article_id"], "section_id": text_row["section_id"], "BertP": bp.item(), "BertR": br.item(), "BertF1": bf1.item()}
       rscores = rouge.score(target=text_row['target_sum'], prediction=text_row['gen_sum'])
       for key in rscores.keys():
         score_row[key+'P'] = rscores[key][0] * 100
@@ -91,8 +91,8 @@ def main():
     
     df = pd.DataFrame.from_dict(res)
     select_sections = ["i", "m", "r", "c"]
-    save_score_df(df, "partial_unfiltered_scores.csv", args)
-    save_score_df(df[df['section_id'].isin(select_sections)], "partial_filtered_scores.csv", args)
+    save_score_df(df.loc[:, df.columns!="section_id"], "partial_unfiltered_scores.csv", args)
+    save_score_df(df[df['section_id'].isin(select_sections)].loc[:, df.columns!="section_id"], "partial_filtered_scores.csv", args)
     df.to_csv(os.path.join(args.data_root, "partial_unfiltered_scores_details.csv"), encoding="utf-8", index=False)
     df[df['section_id'].isin(select_sections)].to_csv(os.path.join(args.data_root, "partial_filtered_scores_details.csv"), encoding="utf-8", index=False)
 
