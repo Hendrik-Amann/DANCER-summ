99be78b503d74a71e4c84dd19b17d5cd30e62cee
Hendrik-Amann <100995431+Hendrik-Amann@users.noreply.github.com>
2023-12-26 09:48:46 +0100

    Update eval.py -include comparison between models

--- src/eval.py
+++ src/eval.py
@@ -7,7 +7,6 @@ import glob
 import pandas as pd
 from bert_score import BERTScorer
 from rouge_score import rouge_scorer
-from datasets import load_metric
 
 with FileLock(".lock") as lock:
   nltk.download("punkt", quiet=True)
@@ -29,6 +28,8 @@ def save_score_df(df, file_name, args):
 def read_args():
   parser = argparse.ArgumentParser()
   parser.add_argument("--data_root", type=str, help="")
+  parser.add_argument("--modelA", type=str, help="")
+  parser.add_argument("--modelB", type=str, help="")
   parser.add_argument("--type", type=str, help="")
   
   args, unknown = parser.parse_known_args()
@@ -41,36 +42,57 @@ def main():
   rouge = rouge_scorer.RougeScorer(rouge_types=["rouge1", "rouge2", "rougeLsum"], use_stemmer=True, split_summaries=True)
   res = []
 
-  if args.type=="full":
-    for ref in glob.glob(os.path.join(args.data_root, "ref/ref_*")):
-      with open(ref) as f:
-        ref_text = f.read()
-      ref_text = re.sub("\n", " ", ref_text)
-      id = ref.split("/")[-1][4:-4]
-      hyp = glob.glob(os.path.join(args.data_root, "hyp", "hyp_"+id+".txt"))
-      if len(hyp) == 0:
-        print("No hyp file for article_id: ", id)
-        continue
-      elif len(hyp) > 1:
-        print("Multiple hyp files for article_id:", id)
-        continue
-      with open(hyp[0]) as f:
-        hyp_text = f.read()
-      hyp_text = re.sub("\n", " ", hyp_text)
+  def score(hyp, ref, id, section_id=None):
+    bp, br, bf1 = bert.score(cands=[hyp], refs=[ref])
 
-      bp, br, bf1 = bert.score(cands=[hyp_text], refs=[ref_text])
+    if section_id:
+      row = {'article_id': id, "section_id": section_id, "BertP": bp.item(), "BertR": br.item(), "BertF1": bf1.item()}  
+    else:
       row = {'article_id': id, "BertP": bp.item(), "BertR": br.item(), "BertF1": bf1.item()}
+    rscores = rouge.score(target=ref, prediction=hyp)
+    for key in rscores.keys():
+      row[key+'P'] = rscores[key][0] * 100
+      row[key+'R'] = rscores[key][1] * 100
+      row[key+'F1'] = rscores[key][2] * 100
 
-      #load_metric is a wrapper around rouge. Used load_metric instead of rouge here since it offers aggregated results
-      metric = load_metric("rouge")
-      
-      rscores = rouge.score(target=ref_text, prediction=hyp_text)
-      for key in rscores.keys():
-        row[key+'P'] = rscores[key][0] * 100
-        row[key+'R'] = rscores[key][1] * 100
-        row[key+'F1'] = rscores[key][2] * 100
+    return row
 
-      res.append(row)
+  if args.type in ("full", "model"):
+    if args.type=="full":
+      for ref in glob.glob(os.path.join(args.data_root, "ref/ref_*")):
+        with open(ref) as f:
+          ref_text = f.read()
+        ref_text = re.sub("\n", " ", ref_text)
+        id = ref.split("/")[-1][4:-4]
+        hyp = glob.glob(os.path.join(args.data_root, "hyp", "hyp_"+id+".txt"))
+        if len(hyp) == 0:
+          print("No hyp file for article_id: ", id)
+          continue
+        elif len(hyp) > 1:
+          print("Multiple hyp files for article_id:", id)
+          continue
+        with open(hyp[0]) as f:
+          hyp_text = f.read()
+        hyp_text = re.sub("\n", " ", hyp_text)
+        res.append(score(hyp_text, ref_text, id))
+
+    else:
+      for hypA in glob.glob(os.path.join(args.modelA, "hyp/hyp_*")):
+        with open(hypA) as f:
+          a_text = f.read()
+        a_text = re.sub("\n", " ", a_text)
+        id = hypA.split("/")[-1][4:-4]
+        hypB = glob.glob(os.path.join(args.modelB, "hyp", "hyp_"+id+".txt"))
+        if len(hypB) == 0:
+          print("No hyp file for model B for article_id: ", id)
+          continue
+        elif len(hypB) > 1:
+          print("Multiple hyp files for model B for article_id: ", id)
+          continue
+        with open(hypB[0]) as f:
+          b_text = f.read()
+        b_text = re.sub("\n", " ", b_text)
+        res.append(score(a_text, b_text, id))
 
     df = pd.DataFrame.from_dict(res)
     save_score_df(df, "scores.csv", args)
@@ -80,14 +102,7 @@ def main():
     tdf = pd.read_csv(os.path.join(args.data_root, "partialSummaries.csv"))
     tdf = tdf.dropna()
     for index, text_row in tdf.iterrows():
-      bp, br, bf1 = bert.score(cands=[text_row['gen_sum']], refs=[text_row['target_sum']])
-      score_row = {"article_id": text_row["article_id"], "section_id": text_row["section_id"], "BertP": bp.item(), "BertR": br.item(), "BertF1": bf1.item()}
-      rscores = rouge.score(target=text_row['target_sum'], prediction=text_row['gen_sum'])
-      for key in rscores.keys():
-        score_row[key+'P'] = rscores[key][0] * 100
-        score_row[key+'R'] = rscores[key][1] * 100
-        score_row[key+'F1'] = rscores[key][2] * 100
-      res.append(score_row)
+      res.append(score(text_row['gen_sum'], text_row['target_sum'], text_row['article_id'], text_row["section_id"]))
     
     df = pd.DataFrame.from_dict(res)
     select_sections = ["i", "m", "r", "c"]
